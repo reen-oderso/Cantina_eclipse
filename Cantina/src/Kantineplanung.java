0742e4fa82bcd3a6df0c9406e6019336e916e95c
reen <reen1337@gmail.com>
2012-12-12 21:31:08 +0100

    Erster Edit

--- Cantina/src/Kantineplanung.java
+++ Cantina/src/Kantineplanung.java
@@ -5,8 +5,8 @@ public class Kantineplanung {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hi Lukas, klappt Git?");
+		System.out.println("Dann kanns ja losgehen");
 	}
 
 }
