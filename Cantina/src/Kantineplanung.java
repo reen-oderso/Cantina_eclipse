c73915f2fb3795f0595eeaab491e6ba35e292f99
reen <reen1337@gmail.com>
2012-12-14 22:12:29 +0100

    Revert "Delete"
    This reverts commit 25df3a99e910cae487abbc85261fc451ac0ae90f.

---
+++ Cantina/src/Kantineplanung.java
@@ -0,0 +1,12 @@
+
+public class Kantineplanung {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi Lukas, klappt Git?");
+		System.out.println("Dann kanns ja losgehen");
+	}
+
+}
---
+++ Cantina/src/Lieferantenverwaltung.java
@@ -0,0 +1,8 @@
+
+public class Lieferantenverwaltung {
+
+	public Lieferantenverwaltung() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
