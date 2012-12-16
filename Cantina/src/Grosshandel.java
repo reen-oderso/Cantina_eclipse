d97f0723ca58f0583861a7f9fa7de5f95a10148f
Lukas <Lukas.Krotki@gmx.net>
2012-12-16 15:37:39 +0100

    Klasse Grosshandel integriert

---
+++ Cantina/src/Grosshandel.java
@@ -0,0 +1,42 @@
+
+/**
+ * Die Objekte der Klasse Grosshandel erben alle ihre wesentlichen Attribute und Methoden von der Klasse Lieferant,
+ * nur die Methode zur Ausgabe des Lieferkostensatz wird hier zusätzlich implementiert.
+ * Die Klasse Grosshandel ist eine Subklasse der Oberklasse Lieferant.
+ * 
+ * @author Rene Wiederhold 
+ * @version
+ */
+public class Grosshandel extends Lieferant
+{
+    private float lieferkostenSatz;     //Der Kostensatz, welcher pro bestellten Artikel fällig wird. 
+
+    /**
+     * Konstruktor für Objekte der Klasse Grosshandel
+     */
+    public Grosshandel()
+    {
+        lieferkostenSatz = 0;
+    }
+    
+     /**
+     * Gibt den Lieferkostensatz eines Grosshandels zurück. Dies wird für die Transportkostenberechnung innerhalb der 
+     * Einkaufsliste benötigt.
+     *
+     * @return    Der Lieferkostensatz des Grosshandels.
+     */
+    public float getLieferkostensatz()
+    {
+        return lieferkostenSatz;
+    }
+    
+    /**
+     * Setzt den Lieferkostensatz eines Grosshandels.
+     *
+     * @param  entf  Der Lieferkostensatz des Grosshandels
+     */
+    public void setLieferkostensatz(float entf)
+    {
+
+    }
+}
